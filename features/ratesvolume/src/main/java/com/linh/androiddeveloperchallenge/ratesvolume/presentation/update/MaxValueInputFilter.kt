20fdc6a8bcff060a239c925b7595b1517d80346b
Linh Van Nguyen <linhvnguyen9@gmail.com>
2022-06-03 17:30:53 +0700

    Fix not checking assign trees limit

---
+++ features/ratesvolume/src/main/java/com/linh/androiddeveloperchallenge/ratesvolume/presentation/update/MaxValueInputFilter.kt
@@ -0,0 +1,17 @@
+package com.linh.androiddeveloperchallenge.ratesvolume.presentation.update
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class MaxValueInputFilter : InputFilter {
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int
+    ): CharSequence {
+        TODO("Not yet implemented")
+    }
+}
